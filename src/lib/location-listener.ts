0ab9deb1c2bc2f304233b08778c08634f28768d9
CaptSiro <honzahorak433@icloud.com>
2023-08-01 13:19:09 +0200

    Extracted location listener

---
+++ src/lib/location-listener.ts
@@ -0,0 +1,28 @@
+import Impulse from "./Impulse";
+
+
+
+export const url = new Impulse<string>();
+
+
+
+export default function bindLocationListener(): void {
+    const observer = new MutationObserver(() => {
+        url.pulse(window.location.href);
+    });
+
+    const title = document.head.querySelector("title");
+    if (title === null) {
+        return;
+    }
+
+    observer.observe(title, {
+        childList: true,
+        subtree: true,
+        characterData: true
+    });
+
+    window.addEventListener('beforeunload', () => {
+        observer.disconnect();
+    });
+}
--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,10 @@
 import getDefaultStore from "./lib/store/get-default-store";
 import Pasterino from "./components/Pasterino/Pasterino";
 import { $ } from "./lib/tungsten/domx";
-import Impulse from "./lib/Impulse";
 import getPlatform from "./platform/get-platform";
+import bindLocationListener from "./lib/location-listener";
 
 
-
-export const url = new Impulse<string>();
 
 export const store = getDefaultStore();
 
@@ -40,23 +38,5 @@ async function main(evt: KeyboardEvent) {
 window.addEventListener("load", () => {
     window.addEventListener("keydown", main);
 
-    const observer = new MutationObserver(() => {
-        console.log(window.location.href);
-        url.pulse(window.location.href);
-    });
-
-    const title = document.head.querySelector("title");
-    if (title === null) {
-        return;
-    }
-
-    observer.observe(title, {
-        childList: true,
-        subtree: true,
-        characterData: true
-    });
-
-    window.addEventListener('beforeunload', () => {
-        observer.disconnect();
-    });
+    bindLocationListener();
 }, { once: true });
