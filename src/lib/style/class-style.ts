dc313108544e84fb1a7bfeb411bfbe591acf7988
CaptSiro <honzahorak433@icloud.com>
2023-08-01 14:04:54 +0200

    Implemented simple style class detector

---
+++ src/lib/style/class-style.ts
@@ -0,0 +1,43 @@
+import { Styles } from "../../@types";
+import mergeStyles from "./merge-styles";
+import { createCSSString } from "../tungsten/jsml";
+
+
+
+const observer = new MutationObserver((mutations) => {
+    for (const mutation of mutations) {
+        const t = mutation.target;
+
+        if (!(t instanceof HTMLElement) || t.dataset.csClass === undefined) {
+            continue;
+        }
+
+        const cssClass = t.dataset.csClass!;
+
+        if (t.classList.contains(cssClass)) {
+            t.setAttribute("style", t.dataset.csBase!);
+            continue;
+        }
+
+        t.setAttribute("style", t.dataset.csStyle!);
+    }
+});
+
+const observerOptions: MutationObserverInit = {
+    attributes: true
+};
+
+
+
+export default function classStyle(cssClass: string, base: Styles, classStyle: Styles): (e: HTMLElement) => void {
+    const compBase = createCSSString(base);
+    const compStyle = createCSSString(mergeStyles(base, classStyle));
+
+    return e => {
+        e.dataset.csClass = cssClass;
+        e.dataset.csBase = compBase;
+        e.dataset.csStyle = compStyle;
+
+        observer.observe(e, observerOptions);
+    }
+}
--- src/lib/style/merge-styles.ts
+++ src/lib/style/merge-styles.ts
@@ -2,8 +2,8 @@ import { Styles } from "../../@types";
 
 
 
-export default function mergeStyles(base: Styles, ...styles: Styles[]) {
-    const copy = { ...base };
+export default function mergeStyles(base: Styles, ...styles: Styles[]): Styles {
+    const copy: Styles = { ...base };
 
     for (let i = 0; i < styles.length; i++) {
         const s = styles[i];
