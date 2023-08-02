73d9a6741cc96ba15568440c28521ea01c436ae6
CaptSiro <honzahorak433@icloud.com>
2023-08-02 14:17:37 +0200

    Implemented word generator

---
+++ src/lib/word-generator.ts
@@ -0,0 +1,15 @@
+export default function* wordGenerator(string: string) {
+    let start = 0;
+
+    for (let i = 0; i < string.length; i++) {
+        if (string.charCodeAt(i) !== 32 && string.charCodeAt(i) !== 160) {
+            if (i === string.length - 1) {
+                yield string.substring(start, i);
+            }
+            continue;
+        }
+
+        yield string.substring(start, i);
+        start = i + 1;
+    }
+}
