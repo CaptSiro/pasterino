66ff91be73584e2e4b20d6fb03bd42dd01fdfa83
CaptSiro <honzahorak433@icloud.com>
2024-09-26 23:33:09 +0200

    Fixed not sending copy-pastas in chat

--- src/platform/twitch.tv/TwitchPlatform.ts
+++ src/platform/twitch.tv/TwitchPlatform.ts
@@ -3,11 +3,8 @@ import Platform from "../Platform";
 
 
 type ChatInput = {
-    props: {
-        onChange(t: {
-            target: { value: string }
-        }): void;
-    }
+    onChange(): void;
+    children: any[];
 }
 
 
@@ -44,9 +41,21 @@ export default class TwitchPlatform implements Platform {
     }
 
     setChatInput(data: string): void {
-        this.getChatInputReact()?.props.onChange({
-            target: { value: data }
-        });
+        const chatInput= this.getChatInputReact();
+        if (chatInput === undefined) {
+            console.warn('[Pasterino]: Could not find chat input editor');
+            return;
+        }
+
+        chatInput.children = [{
+            type: 'paragraph',
+            children: [{
+                type: 'text',
+                text: data
+            }]
+        }];
+
+        chatInput.onChange();
     }
 
     getChatInput(): HTMLElement | undefined {
@@ -54,17 +63,17 @@ export default class TwitchPlatform implements Platform {
     }
 
     // source: 7tv
-    getChatInputReact(): ChatInput {
-        return this.getAutocompleteHandler()?.componentRef;
+    getChatInputReact(): ChatInput | undefined {
+        return this.getAutocompleteHandler();
     }
 
     getAutocompleteHandler() {
         const node = this.findReactChildren(
-            this.getReactInstance(this.getChatInput()),
-            (n: any) => n.stateNode.providers
+            this.getReactInstance(this.getPromptElement()),
+            (n: any) => n.props.node !== undefined
         );
 
-        return node?.stateNode;
+        return node?.props.node;
     }
 
     findReactChildren(node: any, predicate: any, maxDepth = 15, depth = 0): any | null {
@@ -73,9 +82,19 @@ export default class TwitchPlatform implements Platform {
         if (success) return node;
         if (!node || depth > maxDepth) return null;
 
-        const { child, sibling } = node;
-        if (child || sibling) {
-            return this.findReactChildren(child, predicate, maxDepth, depth + 1) || this.findReactChildren(sibling, predicate, maxDepth, depth + 1);
+        if (node.children instanceof Array) {
+            for (const child of node.children) {
+                if (typeof child !== 'object') {
+                    continue;
+                }
+
+                const reactNode = this.findReactChildren(child, predicate, maxDepth, depth + 1);
+                if (reactNode !== null) {
+                    return reactNode;
+                }
+            }
+        } else if (typeof node.children === 'object') {
+            return this.findReactChildren(node.children, predicate, maxDepth, depth + 1);
         }
 
         return null;
@@ -87,7 +106,7 @@ export default class TwitchPlatform implements Platform {
         }
 
         for (const key in element) {
-            if (key.startsWith('__reactInternalInstance$')) {
+            if (key.startsWith('__reactProps$')) {
                 return (element as any)[key] as any;
             }
         }
